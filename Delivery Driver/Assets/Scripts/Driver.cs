74ba7dc9ac3ee45d6da593f5c4106215b5dfde31
linhvu3990 <mrpi3990@gmail.com>
2021-12-29 17:42:56 +0800

    Variable

---
+++ Delivery Driver/Assets/Scripts/Driver.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    float steerSpeed = 1f;
+    float moveSpeed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
+    }
+}
