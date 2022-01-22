822d80d863c366daadf5745c7c2e9d7a881b5597
linhvu3990 <mrpi3990@gmail.com>
2022-01-22 21:41:33 +0800

    Using OnTriggerEnter2D()

--- Delivery Driver/Assets/Scripts/Collision.cs
+++ Delivery Driver/Assets/Scripts/Collision.cs
@@ -7,4 +7,8 @@ public class Collision : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch!");
     }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("What was that?");    
+    }
 }
